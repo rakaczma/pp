75111dfe28db7513e5b028948a97258c5f60980c
rakaczma <rakaczma@student.wszib.edu.pl>
2023-01-15 13:42:32 +0100

    upd6

--- zadanie_domowe_1_3.py
+++ zadanie_domowe_1_3.py
@@ -1,47 +1,32 @@
 own_funds = 30_000.
-factor = 0.075
+deposit = own_funds
+factor1 = 0.075
+factor2 = 0.08
+factor3 = 0.0825
 q1 = 0.25
 q2 = 0.5
 q3 = 0.75
 
-saldo_q1 = round(own_funds + (own_funds * (factor * q1)), 2)
-saldo_q2 = round(saldo_q1 + (saldo_q1 * (factor * q2)), 2)
-saldo_q3 = round(saldo_q2 + (saldo_q2 * (factor * q3)), 2)
-saldo_q4 = round(saldo_q3 + (saldo_q3 * factor), 2)
-
-print(saldo_q1)
-print(saldo_q2)
-print(saldo_q3)
-print(saldo_q4)
-
-print("Roczny zysk z inwestycji wyniesie", saldo_q4, "zł. Saldo po pierwszym kwartale wyniesie", saldo_q1,
-      "zł., po drugim kwartale wyniesie", saldo_q2, "zł., natomiast po trzecim kwartale będzie równe", saldo_q3, "zł.",
-      "\n")
-
-
+# saldo_q1 = round(own_funds + deposit * factor1 * q1, 2)
+# saldo_q2 = round(own_funds + (own_funds + deposit * factor1 * q1) * factor1 * q2, 2)
+# saldo_q3 = round(own_funds + (own_funds + (own_funds + deposit * factor1 * q1) * factor1 * q2) * factor1 * q3, 2)
+# saldo_q4 = round(own_funds + (own_funds + (own_funds + (own_funds + deposit * factor1 * q1) * factor1 * q2) * factor1 * q3) * factor1, 2)
+# profit = round((own_funds + (own_funds + (own_funds + (own_funds + deposit * factor1 * q1) * factor1 * q2) * factor1 * q3) * factor1) - own_funds, 2)
 #
-# deposit1 = own_funds
-# deposit2 = own_funds
-# deposit3 = own_funds
-# factor1 = 1.075
-# factor2 = 1.08
-# factor3 = 1.0825
-# deposit1 *= factor1
-# deposit2 *= factor2
-# deposit3 *= factor3
-# saldo1 = round(deposit1 - own_funds, 2) / 4 + own_funds
-# saldo2 = round(deposit1 - own_funds, 2) / 4 + saldo1
-# saldo3 = round(deposit1 - own_funds, 2) / 4 + saldo2
-# saldo4 = round(deposit2 - own_funds, 2) / 4 + own_funds
-# saldo5 = round(deposit2 - own_funds, 2) / 4 + saldo4
-# saldo6 = round(deposit2 - own_funds, 2) / 4 + saldo5
-# saldo7 = round(deposit3 - own_funds, 2) / 4 + own_funds
-# saldo8 = round(deposit3 - own_funds, 2) / 4 + saldo7
-# saldo9 = round(deposit3 - own_funds, 2) / 4 + saldo8
-#
-# print("Roczny zysk z inwestycji wyniesie", round(deposit1 - own_funds, 2), "zł. Saldo po pierwszym kwartale wyniesie",
-#       saldo1, "zł., po drugim kwartale wyniesie", saldo2, "zł., natomiast po trzecim kwartale będzie równe", saldo3, "zł.", "\n")
-# print("Roczny zysk z inwestycji wyniesie", round(deposit2 - own_funds, 2), "zł. Saldo po pierwszym kwartale wyniesie",
-#       saldo4, "zł., po drugim kwartale wyniesie", saldo5, "zł., natomiast po trzecim kwartale będzie równe", saldo6, "zł.", "\n")
-# print("Roczny zysk z inwestycji wyniesie", round(deposit3 - own_funds, 2), "zł. Saldo po pierwszym kwartale wyniesie",
-#       saldo7, "zł., po drugim kwartale wyniesie", saldo8, "zł., natomiast po trzecim kwartale będzie równe", saldo9, "zł.", "\n")
+# print(saldo_q1)
+# print(saldo_q2)
+# print(saldo_q3)
+# print(saldo_q4)
+# print(profit)
+
+print("Roczny zysk z inwestycji wyniesie", round((own_funds + (own_funds + (own_funds + (own_funds + deposit * factor1 * q1) * factor1 * q2) * factor1 * q3) * factor1) - own_funds, 2), "zł. Saldo po pierwszym kwartale wyniesie", round(own_funds + deposit * factor1 * q1, 2),
+      "zł., po drugim kwartale wyniesie", round(own_funds + (own_funds + deposit * factor1 * q1) * factor1 * q2, 2), "zł., po trzecim kwartale będzie równe", round(own_funds + (own_funds + (own_funds + deposit * factor1 * q1) * factor1 * q2) * factor1 * q3, 2), "zł., natomiast po czwartym kwartale będzie równe",
+      round(own_funds + (own_funds + (own_funds + (own_funds + deposit * factor1 * q1) * factor1 * q2) * factor1 * q3) * factor1, 2), "zł.\n")
+
+print("Roczny zysk z inwestycji wyniesie", round((own_funds + (own_funds + (own_funds + (own_funds + deposit * factor2 * q1) * factor2 * q2) * factor2 * q3) * factor2) - own_funds, 2), "zł. Saldo po pierwszym kwartale wyniesie", round(own_funds + deposit * factor2 * q1, 2),
+      "zł., po drugim kwartale wyniesie", round(own_funds + (own_funds + deposit * factor2 * q1) * factor2 * q2, 2), "zł., po trzecim kwartale będzie równe", round(own_funds + (own_funds + (own_funds + deposit * factor2 * q1) * factor2 * q2) * factor2 * q3, 2), "zł., natomiast po czwartym kwartale będzie równe",
+      round(own_funds + (own_funds + (own_funds + (own_funds + deposit * factor2 * q1) * factor2 * q2) * factor2 * q3) * factor2, 2), "zł.\n")
+
+print("Roczny zysk z inwestycji wyniesie", round((own_funds + (own_funds + (own_funds + (own_funds + deposit * factor3 * q1) * factor3 * q2) * factor3 * q3) * factor3) - own_funds, 2), "zł. Saldo po pierwszym kwartale wyniesie", round(own_funds + deposit * factor3 * q1, 2),
+      "zł., po drugim kwartale wyniesie", round(own_funds + (own_funds + deposit * factor3 * q1) * factor3 * q2, 2), "zł., po trzecim kwartale będzie równe", round(own_funds + (own_funds + (own_funds + deposit * factor3 * q1) * factor3 * q2) * factor3 * q3, 2), "zł., natomiast po czwartym kwartale będzie równe",
+      round(own_funds + (own_funds + (own_funds + (own_funds + deposit * factor3 * q1) * factor3 * q2) * factor3 * q3) * factor3, 2), "zł.\n")
