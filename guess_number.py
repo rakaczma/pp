0a932e73aab9a796fe34fae62ad6b65860d81165
rakaczma <rakaczma@student.wszib.edu.pl>
2023-01-21 11:37:29 +0100

    upd

--- guess_number.py
+++ guess_number.py
@@ -1,5 +1,10 @@
 import random
 
 number = random.randint(1, 3)
+guess = int(input("Zgadnij jaka liczbe mam na mysli (1, 2, 3): "))
+if guess == number:
+    print("Brawo! To mialem na mysli!")
+else:
+    print("niestety myslalem o liczbie " + str(number) + ".")
 
-print(number)
+#print(number)
